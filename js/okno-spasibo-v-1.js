25c733fd317af35dd076cb42465a1d63442ef611
Aliaksei Karzhou <alexius90e@gmail.com>
2025-09-08 12:31:36 +0300

    feat: added perv-ekr-v-01

---
+++ js/okno-spasibo-v-1.js
@@ -0,0 +1,30 @@
+const thanksModal = document.querySelector('.okno-spasibo-v-1');
+const thanksForms = document.querySelectorAll('.form-with-okno-spasibo');
+
+if (thanksModal) {
+  thanksModal.addEventListener('click', (event) => {
+    const isLayout = event.currentTarget === event.target;
+    const isClose = event.target.classList.contains('okno-spasibo-v-1__close-button');
+    if (isLayout || isClose) thanksModal.classList.remove('active');
+  });
+}
+
+thanksForms.forEach((thanksForm) => {
+  thanksForm.addEventListener('submit', (event) => {
+    const url = 'https://example.com';
+
+    event.preventDefault();
+    event.currentTarget.reset();
+
+    if (url) {
+      setTimeout(() => {
+         window.open(url, '_blank');
+      }, 4000)
+     
+    }
+
+    setTimeout(() => {
+      if (thanksModal) thanksModal.classList.add('active');
+    }, 320);
+  });
+});
