ee97a3a380d28a983ca36e9e5350c0007156ec94
Aliaksei Karzhou <alexius90e@gmail.com>
2025-10-21 13:12:30 +0300

    feat: added karusel-izobr-v-06

--- js/karusel-izobr-v-06.js
+++ js/karusel-izobr-v-06.js
@@ -12,8 +12,11 @@ if (karuselIzobrV06SwiperEl) {
       320: {
         spaceBetween: 12,
       },
-      993: {
+      769: {
         spaceBetween: 20,
+      },
+      1501: {
+        spaceBetween: 30,
       },
     },
     navigation: {
@@ -22,3 +25,18 @@ if (karuselIzobrV06SwiperEl) {
     },
   });
 }
+
+const karuselIzobrV06Slide = document.querySelector('.karusel-izobr-v-06__slide');
+const karuselIzobrV06Prev = document.querySelector('.karusel-izobr-v-06 .swiper-button-next');
+const karuselIzobrV06Next = document.querySelector('.karusel-izobr-v-06 .swiper-button-prev');
+
+const updateKaruselIzobrV06ButtonsTop = () => {
+  if (karuselIzobrV06Slide && karuselIzobrV06Prev && karuselIzobrV06Next) {
+    karuselIzobrV06Prev.style.top = `${karuselIzobrV06Slide.offsetWidth / 2}px`;
+    karuselIzobrV06Next.style.top = `${karuselIzobrV06Slide.offsetWidth / 2}px`;
+  }
+};
+
+window.addEventListener('resize', updateKaruselIzobrV06ButtonsTop);
+
+updateKaruselIzobrV06ButtonsTop();
