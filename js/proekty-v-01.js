bcd96033e493a5dbc291a03ea908eb5009d9a6ea
Aliaksei Karzhou <alexius90e@gmail.com>
2025-10-22 23:04:08 +0300

    feat: added proekty-v-01

---
+++ js/proekty-v-01.js
@@ -0,0 +1,70 @@
+const proektyV5CardSliders = document.querySelectorAll('.proekty-v-01__card-sliders');
+
+proektyV5CardSliders.forEach((slidersElem) => {
+  const baseClassName = '.proekty-v-01__card-sliders';
+  const mainSliderElem = slidersElem.querySelector(`${baseClassName}-main .swiper`);
+  const thumbsSliderElem = slidersElem.querySelector(`${baseClassName}-thumbs .swiper`);
+  const mainSliderNext = slidersElem.querySelector(`${baseClassName}-main-controls-next`);
+  const mainSliderPrev = slidersElem.querySelector(`${baseClassName}-main-controls-prev`);
+
+  if (mainSliderElem && thumbsSliderElem) {
+    const thumbsSwiper = new Swiper(thumbsSliderElem, {
+      spaceBetween: 0,
+      slidesPerView: 3,
+      freeMode: true,
+      watchSlidesProgress: true,
+      allowTouchMove: false,
+    });
+    const mainSwiper = new Swiper(mainSliderElem, {
+      spaceBetween: 20,
+      slidesPerView: 1,
+      loop: true,
+      navigation: {
+        nextEl: mainSliderNext,
+        prevEl: mainSliderPrev,
+      },
+      thumbs: {
+        swiper: thumbsSwiper,
+      },
+    });
+  }
+});
+
+const proektyV5Modal = document.querySelector('.proekty-v-01__modal');
+const proektyV5ModalForm = document.querySelector('.proekty-v-01__modal-form');
+const proektyV5ContactButtons = document.querySelectorAll(
+  '.proekty-v-01__card-content-contact-button'
+);
+
+if (proektyV5Modal && proektyV5ModalForm) {
+  proektyV5ContactButtons.forEach((button) => {
+    button.addEventListener('click', () => {
+      proektyV5Modal.classList.add('active');
+    });
+  });
+
+  proektyV5Modal.addEventListener('click', (event) => {
+    const isLayout = event.currentTarget === event.target;
+    const isClose = event.target.classList.contains('proekty-v-01__modal-close-button');
+    if (isLayout || isClose) proektyV5Modal.classList.remove('active');
+  });
+
+  proektyV5ModalForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    proektyV5Modal.classList.remove('active');
+  });
+}
+
+const proektyV5More = document.querySelector('.proekty-v-01__more');
+
+if (proektyV5More) {
+  proektyV5More.addEventListener('click', (event) => {
+    const isButton = event.target.classList.contains('proekty-v-01__more-button');
+
+    if (isButton) {
+      const hiddenCards = document.querySelectorAll('.proekty-v-01__card.hidden');
+      hiddenCards.forEach((card) => card.classList.remove('hidden'));
+      event.currentTarget.style.display = 'none';
+    }
+  });
+}
