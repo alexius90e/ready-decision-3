859c9120646f0f9d08471f74b5077dc22f47100f
Aliaksei Karzhou <alexius90e@gmail.com>
2025-11-12 18:49:38 +0300

    fix: fixed style issues

--- js/taby-v-02.js
+++ js/taby-v-02.js
@@ -3,14 +3,5 @@ const tabyV02SwiperEl = document.querySelector('.taby-v-02 .swiper');
 if (tabyV02SwiperEl) {
   const tabyV02Swiper = new Swiper(tabyV02SwiperEl, {
     slidesPerView: 'auto',
-    spaceBetween: 15,
-    breakpoints: {
-      320: {
-        spaceBetween: 14,
-      },
-      993: {
-        spaceBetween: 16,
-      },
-    },
   });
 }
