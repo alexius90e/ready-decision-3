e119fc3cf0a80d63a9111c2cfcee20a6d4ef64b1
Aliaksei Karzhou <alexius90e@gmail.com>
2026-04-27 01:45:23 +0300

    feat: added search-01

--- js/search-01.js
+++ js/search-01.js
@@ -1,24 +1,26 @@
 const search01InputField = document.querySelector('.search-01__input');
 const search01ResetButton = document.querySelector('.search-01__reset');
 
-const togglesearch01ResetButton = () => {
-  const hasText = search01InputField.value.length > 0;
-  if (hasText) {
-    search01ResetButton.classList.add('active');
-  } else {
+if (search01InputField && search01ResetButton) {
+  const togglesearch01ResetButton = () => {
+    const hasText = search01InputField.value.length > 0;
+    if (hasText) {
+      search01ResetButton.classList.add('active');
+    } else {
+      search01ResetButton.classList.remove('active');
+    }
+  };
+
+  search01InputField.addEventListener('input', togglesearch01ResetButton);
+
+  const handleReset = () => {
+    search01InputField.value = '';
     search01ResetButton.classList.remove('active');
-  }
-};
+    search01InputField.focus();
+    togglesearch01ResetButton();
+  };
 
-search01InputField.addEventListener('input', togglesearch01ResetButton);
+  search01ResetButton.addEventListener('click', handleReset);
 
-const handleReset = () => {
-  search01InputField.value = '';
-  search01ResetButton.classList.remove('active');
-  search01InputField.focus();
   togglesearch01ResetButton();
-};
-
-search01ResetButton.addEventListener('click', handleReset);
-
-togglesearch01ResetButton();
+}
