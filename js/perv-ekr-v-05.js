d0e4e8c7c5ba3b1dff696a89c8a503dab39b3e1d
Aliaksei Karzhou <alexius90e@gmail.com>
2026-05-06 23:47:28 +0300

    feat: added perv-ekr-v-05

--- js/perv-ekr-v-05.js
+++ js/perv-ekr-v-05.js
@@ -3,7 +3,7 @@ const pervEkrV05Swiper = document.querySelector('.perv-ekr-v-05__slider .swiper'
 if (pervEkrV05Swiper) {
   const swiper = new Swiper(pervEkrV05Swiper, {
     slidesPerView: 1,
-    spaceBetween: 20,
+    // spaceBetween: 20,
     loop: true,
     autoplay: {
       delay: 4000,
