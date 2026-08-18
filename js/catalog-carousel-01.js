4c2c23ad17ddcb13c46966fcc4764d84d34d85a0
Aliaksei Karzhou <alexius90e@gmail.com>
2026-08-18 12:00:01 +0300

    feat: added catalog-carousel-01

---
+++ js/catalog-carousel-01.js
@@ -0,0 +1,52 @@
+const catalogCarousel01SwiperEl = document.querySelector('.catalog-carousel-01 .swiper');
+const catalogCarousel01SwiperPrevEl = document.querySelector(
+  '.catalog-carousel-01 .catalog-carousel-01__slider-controls-button-prev',
+);
+const catalogCarousel01SwiperNextEl = document.querySelector(
+  '.catalog-carousel-01 .catalog-carousel-01__slider-controls-button-next',
+);
+
+if (catalogCarousel01SwiperEl) {
+  const swiper = new Swiper(catalogCarousel01SwiperEl, {
+    slidesPerView: 'auto',
+    spaceBetween: 24,
+    loop: true,
+    touchEventsTarget: 'container',
+    navigation: {
+      prevEl: catalogCarousel01SwiperPrevEl,
+      nextEl: catalogCarousel01SwiperNextEl,
+    },
+  });
+}
+
+const catalogCarousel01Cards = document.querySelectorAll('.catalog-carousel-01__card');
+
+catalogCarousel01Cards.forEach((card) => {
+  const baseClassName = '.catalog-carousel-01__card';
+  const mainSliderElem = card.querySelector(`${baseClassName}-main .swiper`);
+  const thumbsSliderElem = card.querySelector(`${baseClassName}-thumbs .swiper`);
+
+  if (mainSliderElem && thumbsSliderElem) {
+    const thumbsSwiper = new Swiper(thumbsSliderElem, {
+      loop: true,
+      slidesPerView: 4,
+      freeMode: true,
+      watchSlidesProgress: true,
+      allowTouchMove: false,
+    });
+    const mainSwiper = new Swiper(mainSliderElem, {
+      spaceBetween: 20,
+      loop: true,
+      thumbs: {
+        swiper: thumbsSwiper,
+      },
+    });
+  }
+
+  card.addEventListener('click', (event) => {
+    const isFavButton = event.target.classList.contains(
+      'catalog-carousel-01__card-content-favourites-button',
+    );
+    if (isFavButton) event.target.classList.toggle('active');
+  });
+});
